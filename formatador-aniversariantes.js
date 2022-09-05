81d3ae4cc3dfe212dc80bb42eaad3f654fc0be91
PedroPiuma <luisppiuma@gmail.com>
2022-09-05 18:27:39 -0300

    fixs position tag <table></table>

--- formatador-aniversariantes.js
+++ formatador-aniversariantes.js
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from 'fs'
-let htmlTextFinal = ''
-let htmlTextImages = '<table>'
+let htmlTextFinal = '<table>'
+let htmlTextImages = ''
 let htmlTextInfos = ''
 const thisYear = new Date().getFullYear()
 const writeHtml = () => writeFile('conteudo-final.html', htmlTextFinal.trim(), error => error ? new Error(error) : console.log('Script executado com sucesso! Verifique o arquivo conteudo-final.html :)\n'))
@@ -8,14 +8,8 @@ const readCsv = formatText => readFile('aniversariantes.csv', 'utf-8', (error, t
 const createPhoto = (firstName, lastName) => `<td width="150"><img style="width: 120px; height: 160px; display: block; margin-left: auto; margin-right: auto; border-radius: 10%; border: 1px solid; border-color: #ab9b6a;" src="https://arquivos.essentialnutrition.com.br/images/aniversarios/${thisYear}/julho/${firstName}${lastName}.jpg" alt="" /></td>`
 const createPersonDescription = (firstName, lastName, sector, local, birthday) => `<td width="150"><p style="text-align: center; font-family: arial, sans-serif; color: #1a1a1a; font-size: 12px; line-height: 18px; margin-bottom: 30px;"><b>Dia ${birthday}<br /></b>${firstName.normalize('NFD').replace(/[\u0300-\u036f]/g, "")} ${lastName.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}<br /> ${sector} <br /> ${local}</p></td>`
 const upperCaseFormatterName = text => text[0]?.toUpperCase() + text.slice(1)
-const upperCaseFormatterSector = text => {
-    if (text.length <= 3) return text
-    else return text.split(' ').map(word => upperCaseFormatterName(word.toLowerCase())).join(' ')
-}
-const upperCaseFormatterLocal = text => {
-    if (text.length <= 5) return text.toUpperCase()
-    else return text.split(' ').map(word => word.length > 3 ? upperCaseFormatterName(word.toLowerCase()) : word.toUpperCase()).join(' ')
-}
+const upperCaseFormatterSector = text => text.length <= 3 ? text : text.split(' ').map(word => upperCaseFormatterName(word.toLowerCase())).join(' ')
+const upperCaseFormatterLocal = text => text.length <= 5 ? text.toUpperCase() : text.split(' ').map(word => word.length > 3 ? upperCaseFormatterName(word.toLowerCase()) : word.toUpperCase()).join(' ')
 
 const formatText = text => {
     const objectPerson = text.split(',,\r\n').map(person => person.split(',').filter(el => el !== ''))
@@ -48,11 +42,11 @@ const formatText = text => {
         createDescriptionRows()
         const imageRows = htmlTextImages.split('|')
         const descriptionRows = htmlTextInfos.split('|')
-        const numberOfRows = imageRows.length === descriptionRows.length ? imageRows.length : new Error('Image rows lenght is diferent of description rows lenght.')
+        const numberOfRows = imageRows.length === descriptionRows.length ? imageRows.length : new Error('Image rows length is diferent of description rows length.')
         for (let i = 0; i < numberOfRows; i++) { htmlTextFinal += imageRows[i] + descriptionRows[i] }
     }
     matchRows()
-    htmlTextFinal += '</<table>'
+    htmlTextFinal += '</table>'
     writeHtml()
 }
 readCsv(formatText)
